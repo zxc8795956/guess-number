6f72a18eab85376dea4bc0717de155bb913431f9
zxc8795956 <kobeismvpthebest@gmail.com>
2019-12-02 13:11:27 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,6 +1,10 @@
 import random
+start = input('請決定隨機變數開始值')
+end = input('請決定隨機變數結束值')
+start = int(start)
+end = int(end)
 count = 0 
-r = random.randint(1,100)
+r = random.randint(start,end)
 while True:
 	count = count + 1#count += 1
 	number = input('請猜數字:')
